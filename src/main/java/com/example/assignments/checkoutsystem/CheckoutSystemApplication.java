a9cc48bddaba453ba69bc3bfd6ba3d5e1e342b61
abagri <ashish.bagri@ymail.com>
2023-06-19 03:08:12 +0800

    Initial code

---
+++ src/main/java/com/example/assignments/checkoutsystem/CheckoutSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.assignments.checkoutsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckoutSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckoutSystemApplication.class, args);
+	}
+
+}
