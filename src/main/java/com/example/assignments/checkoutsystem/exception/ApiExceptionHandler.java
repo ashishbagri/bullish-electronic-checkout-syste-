f13ec4b64205aaa8309abb9a49ca498b3f760da5
abagri <ashish.bagri@ymail.com>
2023-06-20 23:06:16 +0800

    Initial code

--- src/main/java/com/example/assignments/checkoutsystem/exception/ApiExceptionHandler.java
+++ src/main/java/com/example/assignments/checkoutsystem/exception/ApiExceptionHandler.java
@@ -1,8 +1,5 @@
 package com.example.assignments.checkoutsystem.exception;
 
-import com.example.assignments.checkoutsystem.model.ErrorResponse;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
